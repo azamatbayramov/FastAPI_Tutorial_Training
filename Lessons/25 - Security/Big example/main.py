8986a11d7ca4f1f31184b8ff1a880b4b3641e23b
Azamat Bayramov <bayramov.azamat04@gmail.com>
2023-06-06 17:19:05 +0300

    Edit file by Lesson 25.3

--- Lessons/25 - Security/Big example/main.py
+++ Lessons/25 - Security/Big example/main.py
@@ -14,8 +14,25 @@ class Token(BaseModel):
     token: str
 
 
-@app.get("/my_token", response_model=Token)
-def get_my_token(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {
-        "token": token
-    }
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+def fake_decode_token(token: str) -> User:
+    return User(
+        username=token + "_fake_decoded",
+        email="john@example.com",
+        full_name="John Doe"
+    )
+
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> User:
+    return fake_decode_token(token)
+
+
+@app.get("/me", response_model=User)
+def get_my_token(current_user: Annotated[User, Depends(get_current_user)]):
+    return current_user
